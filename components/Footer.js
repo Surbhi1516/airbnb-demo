b234a1a1a86f5d94a0a251d26f053cfc8ed95e1f
Surbhi <surbhi1516@gmail.com>
2022-07-28 02:39:31 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>Airbnb Works</p>
+        <p>Newsrooms</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>Airbnb Works</p>
+        <p>Newsrooms</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>Airbnb Works</p>
+        <p>Newsrooms</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800 '>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>Airbnb Works</p>
+        <p>Newsrooms</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb luxe</p>
+      </div>
+      <div className='flex flex-col col-span-4 mx-auto'>
+        <h4 >Copyright Statement</h4>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ pages/search.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import { useRouter } from 'next/router';
+import { format } from 'date-fns';
+import InfoCard from '../components/InfoCard';
+
+const Search = ({ searchResult }) => {
+
+  const router = useRouter();
+
+  const { location, startDate, endDate, guests } = router.query;
+
+  // const formattedStartDate = format(new Date(startDate), 'dd MMMM yy');
+  // const formattedEndDate = format(new Date(endDate), 'dd MMMM yy');
+  // const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+  // console.log(startDate, endDate, range);
+
+  return (
+    <div className='h-screen'>
+      <Header placeholder={`${location} | ${guests}`} />
+      <main>
+        <section className='flex-grow pt-14 px-6'>
+          <p className='text-s '>300+ stays - - for {guests} guests</p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
+          <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap' >
+            <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Cancellation Flexibility</p>
+            <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Type of Place</p>
+            <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Price</p>
+            <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Rooms and Beds</p>
+            <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>More Filters</p>
+          </div>
+
+          {searchResult.map(item => (
+            <InfoCard
+              img={item.img}
+              location={item.location}
+              title={item.title}
+              description={item.description}
+              star={star}
+              price={price}
+              total={total}
+            />
+          ))}
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResult = await fetch('https://links.papareact.com/isz').then(res => res.json());
+
+  return {
+    props: {
+      searchResult,
+    },
+  }
+}
