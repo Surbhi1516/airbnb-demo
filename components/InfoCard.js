8c24a70c0e832dd69f2fc7fbd67bac1de472c7d4
Surbhi <surbhi1516@gmail.com>
2022-08-01 02:05:59 +0530

    day two build

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { HeartIcon } from '@heroicons/react/outline';
+import { StarIcon } from '@heroicons/react/solid';
 
-const InfoCard = () => {
+const InfoCard = ({ img, location, title, description, star, price, total }) => {
   return (
-    <div>InfoCard</div>
+    <div className=' flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg pr-4 transition duration-200 ease-out first:border-t'>
+      <div className='relative h-28 w-44 md:h-56 md:w-80 flex-shrink-0 '>
+        <Image src={img} layout='fill' objectFit='cover' className=' rounded-2xl' />
+      </div>
+      <div className='flex flex-col flex-grow pl-5'>
+        <div className='flex justify-between'>
+          <p>{location}</p>
+          <HeartIcon className='h-7 cursor-pointer' />
+        </div>
+        <h4 className=' text-xl'>{title}</h4>
+        <div className='border-b pt-2 w-28' />
+        <p className=' pt-2 text-sm text-gray-500 flex-grow'>{description}</p>
+        <div className=' flex justify-between  items-end pt-5'>
+          <p className='flex items-center'>
+            <StarIcon className=' h-5 text-red-400' />{star}
+          </p>
+          <div>
+            <p className=' text-lg lg:text-2xl font-semibold pb-2'>{price}</p>
+            <p className=' text-right font-extralight'>{total}</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- pages/search.js
+++ pages/search.js
@@ -25,24 +25,27 @@ const Search = ({ searchResult }) => {
           <p className='text-s '>300+ stays - - for {guests} guests</p>
           <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
           <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap' >
-            <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Cancellation Flexibility</p>
-            <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Type of Place</p>
+            <p className='text-gray-500 px-4 py-2 text-xl border bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Cancellation Flexibility</p>
+            <p className='text-gray-500 px-4 py-2 text-xl border-transparent bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Type of Place</p>
             <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Price</p>
             <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>Rooms and Beds</p>
             <p className='text-gray-500 px-4 py-2 text-xl bg-gray-50 rounded-full  cursor-pointer  hover:shadow-lg   active:scale-95 active:bg-gray-100 transition duration-150 hover:border ease-out'>More Filters</p>
           </div>
+          <div className='flex flex-col'>
 
-          {searchResult.map(item => (
-            <InfoCard
-              img={item.img}
-              location={item.location}
-              title={item.title}
-              description={item.description}
-              star={star}
-              price={price}
-              total={total}
-            />
-          ))}
+            {searchResult.map(item => (
+              <InfoCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                title={item.title}
+                description={item.description}
+                star={item.star}
+                price={item.price}
+                total={item.total}
+              />
+            ))}
+          </div>
         </section>
       </main>
       <Footer />
